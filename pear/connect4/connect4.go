56e532a42ed12e08163359a24c342db0e7b4fc86
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-05-23 13:15:32 +0200

    comments

--- pear/connect4/connect4.go
+++ pear/connect4/connect4.go
@@ -11,6 +11,7 @@ type state struct {
 	board [][]string
 }
 
+// intantiate new game state
 func newGame() state {
 	newState := state{} 
 	newState.turn = 0
@@ -25,6 +26,7 @@ func newGame() state {
 	return newState
 }
 
+// print board to stdout
 func (s state) displayBoard(showColumn bool) {
 	if showColumn{
 		fmt.Println(" 1  2  3  4  5  6  7 ")
@@ -38,17 +40,22 @@ func (s state) displayBoard(showColumn bool) {
 	}
 }
 
+// get move (row) from stdin
 func (s state) getMove() {
+	// player turn
 	turn := s.turn % 2 + 1
 	fmt.Printf("Player %d's turn: ", turn)
 
+	// get input
 	var column int
 	fmt.Scanln(&column)
     
 	for (column > 7 || column < 1) {
+		// flush input buffer
 		var discard string
 		fmt.Scanln(&discard)
 
+		// get input again
 		fmt.Printf("Invalid input. Player %d's turn: ", turn)
 		fmt.Scanln(&column)
 	}
