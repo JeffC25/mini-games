e9ae64a3789949f450b7d667bac0940e43f0d29c
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-05-23 10:54:38 +0200

    move board printing to function

--- pear/connect4.go
+++ pear/connect4.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-	board := []string{"-------", "-------", "-------", "-------", "-------", "-------",}
-
+func printBoard(board []string) {
 	for i := 0; i < len(board); i++ {
 		for j := 0; j < len(board[i]); j++ {
 			fmt.Printf("%c ", board[i][j])
 		}
 		fmt.Println();
 	}
+}
+
+func main() {
+	board := []string{"-------", "-------", "-------", "-------", "-------", "-------",}
+
+	printBoard(board)
 }
