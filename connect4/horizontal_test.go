1b4434f0d95b420453a2997b30dedef7ff5acac2
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-06-01 21:50:10 +0800

    test false positives

--- connect4/horizontal_test.go
+++ connect4/horizontal_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+func TestHorizontal(t *testing.T) {
+	// set up
+	testState := state{}
+	testState.turn = 1
+
+	// test false positive
+	testState.board = [][]int8{
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+	}
+	
+	for i := 0; i < 6; i++ {
+		for turn := 1; turn < 3; turn++ {
+			testState.turn = int8(turn)
+			if testState.checkHorizontal(int8(i)) {
+				t.Error("False positive")
+			}
+		}
+	}
+}
