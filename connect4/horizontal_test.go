5a890ec442f4233afdb88ff4cea5fd3fbb5433cb
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2023-06-01 21:56:51 +0800

    more tests

--- connect4/horizontal_test.go
+++ connect4/horizontal_test.go
@@ -44,13 +44,21 @@ func TestHorizontal(t *testing.T) {
 		}
 	}
 
-	// // test horizontal
-	// testState.board = [][]int8{
-	// 	[]int8{1, 1, 1, 1, 1, 1, 1},
-	// 	[]int8{2, 2, 2, 2, 2, 2, 2},
-	// 	[]int8{1, 1, 1, 1, 1, 1, 1},
-	// 	[]int8{0, 0, 0, 0, 0, 0, 0},
-	// 	[]int8{1, 1, 1, 1, 1, 1, 1},
-	// 	[]int8{0, 0, 0, 0, 0, 0, 0},
-	// }
+	testState.board = [][]int8{
+		[]int8{1, 1, 1, 0, 1, 1, 1},
+		[]int8{0, 2, 0, 2, 0, 2, 0},
+		[]int8{2, 1, 2, 1, 0, 0, 0},
+		[]int8{0, 0, 0, 1, 1, 1, 2},
+		[]int8{0, 0, 0, 0, 0, 0, 0},
+		[]int8{0, 0, 2, 0, 2, 0, 0},
+	}
+	
+	for i := 0; i < 6; i++ {
+		for turn := 1; turn < 3; turn++ {
+			testState.turn = int8(turn)
+			if testState.checkHorizontal(int8(i)) {
+				t.Error("False positive")
+			}
+		}
+	}
 }
